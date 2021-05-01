0cfabd5a7e3be47804359c09647c189b64231fee
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2021-05-01 19:50:00 +0200

    Move return to the try block to prevent throwing psl does not exist error

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function throwTypeError() {
-    throw new TypeError('The given URL is not a string. Please verify your string|array.');
+    throw new TypeError(
+        "The given URL is not a string. Please verify your string|array."
+    );
 }
 
-const endings = ['/', ':', '?', '#'];
-const starters = ['.', '/', '@'];
+const endings = ["/", ":", "?", "#"];
+const starters = [".", "/", "@"];
 
 /**
  * Options to extract domain.
@@ -17,7 +19,7 @@ const starters = ['.', '/', '@'];
  * @returns {string}
  */
 function getDomainFromUrl(url, opts) {
-    if (typeof url !== 'string') {
+    if (typeof url !== "string") {
         throwTypeError();
     }
 
@@ -34,7 +36,7 @@ function getDomainFromUrl(url, opts) {
             break;
         }
 
-        if (url[i] !== '.') {
+        if (url[i] !== ".") {
             continue;
         }
 
@@ -61,13 +63,13 @@ function getDomainFromUrl(url, opts) {
 
     // offsetStartSlice should always be larger than protocol
     if (offsetStartSlice < 2) {
-        return '';
+        return "";
     }
 
     // Very customized if statement for tlds
     if (opts.tld) {
         let offsetStart = 0;
-        const starters = ['/', '@'];
+        const starters = ["/", "@"];
 
         while (i--) {
             if (starters.indexOf(url[i]) > -1) {
@@ -78,14 +80,14 @@ function getDomainFromUrl(url, opts) {
         }
 
         try {
-            const psl = require('psl');
+            const psl = require("psl");
+
+            return psl.get(url.slice(offsetStart, offsetPath));
         } catch (_e) {
             throw Error(
-                'You must install psl library (https://www.npmjs.com/package/psl) to use `tld` option'
+                "You must install psl library (https://www.npmjs.com/package/psl) to use `tld` option"
             );
         }
-
-        return psl.get(url.slice(offsetStart, offsetPath));
     }
 
     // Tried several approaches slicing a string. Can't get it any faster than this.
@@ -99,7 +101,7 @@ function getDomainFromUrl(url, opts) {
  * @returns {string}
  */
 module.exports = function extractDomain(urls, opts = {}) {
-    if (typeof urls === 'string') {
+    if (typeof urls === "string") {
         return getDomainFromUrl(urls, opts);
     } else if (Array.isArray(urls)) {
         const extractedUrls = [];
