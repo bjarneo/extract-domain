79e58b1faec8f9914b90d2c51cf27e0e61e7ecc0
bjarneo <bjarne.oeverli@gmail.com>
2021-12-02 17:20:10 +0100

    chore: add prettierrc and reformat the code base

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function throwTypeError() {
-    throw new TypeError(
-        "The given URL is not a string. Please verify your string|array."
-    );
+    throw new TypeError('The given URL is not a string. Please verify your string|array.');
 }
 
-const endings = ["/", ":", "?", "#"];
-const starters = [".", "/", "@"];
+const endings = ['/', ':', '?', '#'];
+const starters = ['.', '/', '@'];
 
 /**
  * Options to extract domain.
@@ -19,7 +17,7 @@ const starters = [".", "/", "@"];
  * @returns {string}
  */
 function getDomainFromUrl(url, opts) {
-    if (typeof url !== "string") {
+    if (typeof url !== 'string') {
         throwTypeError();
     }
 
@@ -36,7 +34,7 @@ function getDomainFromUrl(url, opts) {
             break;
         }
 
-        if (url[i] !== ".") {
+        if (url[i] !== '.') {
             continue;
         }
 
@@ -66,13 +64,13 @@ function getDomainFromUrl(url, opts) {
     }
 
     if (offsetStartSlice > 0 && offsetStartSlice < 2) {
-        return "";
+        return '';
     }
 
     // Very customized if statement for tlds
     if (opts.tld) {
         let offsetStart = 0;
-        const starters = ["/", "@"];
+        const starters = ['/', '@'];
         let i = offsetDomain;
 
         while (i--) {
@@ -84,12 +82,12 @@ function getDomainFromUrl(url, opts) {
         }
 
         try {
-            const psl = require("psl");
+            const psl = require('psl');
 
             return psl.get(url.slice(offsetStart, offsetPath));
         } catch (_e) {
             throw Error(
-                "You must install psl library (https://www.npmjs.com/package/psl) to use `tld` option"
+                'You must install psl library (https://www.npmjs.com/package/psl) to use `tld` option'
             );
         }
     }
@@ -105,7 +103,7 @@ function getDomainFromUrl(url, opts) {
  * @returns {string}
  */
 module.exports = function extractDomain(urls, opts = {}) {
-    if (typeof urls === "string") {
+    if (typeof urls === 'string') {
         return getDomainFromUrl(urls, opts);
     } else if (Array.isArray(urls)) {
         const extractedUrls = [];
--- index.test.js
+++ index.test.js
@@ -1,44 +1,44 @@
-"use strict";
-const assert = require("assert");
+'use strict';
+const assert = require('assert');
 
 let extractDomain;
 
-if (process.env.NODE_ENV === "travis") {
-    extractDomain = require("./dist/extract-domain");
+if (process.env.NODE_ENV === 'travis') {
+    extractDomain = require('./dist/extract-domain');
 } else {
-    extractDomain = require("./index");
+    extractDomain = require('./index');
 }
 
 const urls = [
-    "https://www.npmjs.com/package/extract-domain",
-    "http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
-    "https://npmjs.com/package/extract-domain",
-    "http://example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
-    "http://www.so.many.sub.domains.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
-    "http://user:password@example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
-    "ftp://example.org/resource.txt",
-    "http://www.npmjs.com",
-    "http://www.npmjs.com?query=test",
-    "http://www.npmjs.com#fragment",
-    "this.is.my@email.com",
-    "test@something.com"
+    'https://www.npmjs.com/package/extract-domain',
+    'http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
+    'https://npmjs.com/package/extract-domain',
+    'http://example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
+    'http://www.so.many.sub.domains.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
+    'http://user:password@example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
+    'ftp://example.org/resource.txt',
+    'http://www.npmjs.com',
+    'http://www.npmjs.com?query=test',
+    'http://www.npmjs.com#fragment',
+    'this.is.my@email.com',
+    'test@something.com',
 ];
 
 const expected = [
-    "npmjs.com",
-    "example.com",
-    "npmjs.com",
-    "example.org",
-    "email.com",
-    "something.com"
+    'npmjs.com',
+    'example.com',
+    'npmjs.com',
+    'example.org',
+    'email.com',
+    'something.com',
 ];
 
-describe("extract domain", () => {
-    it("should return the domain if it already has been extracted", () => {
-        assert.strictEqual(extractDomain("example.com"), "example.com");
+describe('extract domain', () => {
+    it('should return the domain if it already has been extracted', () => {
+        assert.strictEqual(extractDomain('example.com'), 'example.com');
     });
 
-    it("should extract given domain from url", () => {
+    it('should extract given domain from url', () => {
         assert.strictEqual(extractDomain(urls[0]), expected[0]);
         assert.strictEqual(extractDomain(urls[1]), expected[1]);
         assert.strictEqual(extractDomain(urls[7]), expected[0]);
@@ -46,70 +46,67 @@ describe("extract domain", () => {
         assert.strictEqual(extractDomain(urls[10]), expected[4]);
     });
 
-    it("should extract given domain from an array of urls", () => {
+    it('should extract given domain from an array of urls', () => {
         const domains = extractDomain(urls);
 
-        domains.map(domain => assert(expected.indexOf(domain) > -1));
+        domains.map((domain) => assert(expected.indexOf(domain) > -1));
     });
 
-    it("should return empty string if it is not a url", () => {
-        assert.strictEqual(extractDomain("/i.am/just.astring//7test"), "");
+    it('should return empty string if it is not a url', () => {
+        assert.strictEqual(extractDomain('/i.am/just.astring//7test'), '');
     });
 
-    it("should throw syntax error exception if the argument is not string nor array", () => {
+    it('should throw syntax error exception if the argument is not string nor array', () => {
         try {
             extractDomain({});
         } catch (e) {
-            assert.strictEqual(e.name, "TypeError");
+            assert.strictEqual(e.name, 'TypeError');
 
             assert.strictEqual(
                 e.message,
-                "The given URL is not a string. Please verify your string|array."
+                'The given URL is not a string. Please verify your string|array.'
             );
         }
     });
 
-    it("should throw syntax error exception if the array value is not a string", () => {
+    it('should throw syntax error exception if the array value is not a string', () => {
         try {
-            extractDomain([["wow"]]);
+            extractDomain([['wow']]);
         } catch (e) {
-            assert.strictEqual(e.name, "TypeError");
+            assert.strictEqual(e.name, 'TypeError');
 
             assert.strictEqual(
                 e.message,
-                "The given URL is not a string. Please verify your string|array."
+                'The given URL is not a string. Please verify your string|array.'
             );
         }
     });
 
-    it("should support tld if options flag is used", () => {
+    it('should support tld if options flag is used', () => {
         assert.strictEqual(
             extractDomain(
-                "http://www.so.many.sub.domains.example.co.uk:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
+                'http://www.so.many.sub.domains.example.co.uk:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
                 { tld: true }
             ),
-            "example.co.uk"
+            'example.co.uk'
         );
 
         assert.strictEqual(
             extractDomain(
-                "http://user:password@www.so.many.sub.domains.example.co.uk:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
+                'http://user:password@www.so.many.sub.domains.example.co.uk:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
                 { tld: true }
             ),
-            "example.co.uk"
+            'example.co.uk'
         );
 
         assert.strictEqual(
             extractDomain(
-                "http://user:password@www.so.many.sub.domains.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument",
+                'http://user:password@www.so.many.sub.domains.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument',
                 { tld: true }
             ),
-            "example.com"
+            'example.com'
         );
 
-        assert.strictEqual(
-            extractDomain("https://example.com", { tld: true }),
-            "example.com"
-        );
+        assert.strictEqual(extractDomain('https://example.com', { tld: true }), 'example.com');
     });
 });
